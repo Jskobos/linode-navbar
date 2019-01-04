dbd9b1854d6748e0bb473f5d6a77d519ad0ea3b8
Jared Kobos <jaredkobos@gmail.com>
2019-01-04 18:06:21 -0500

    Implement shadow dom with ReactShadow

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import ShadowDOM from 'react-shadow';
 import '../styles/index.css';
 
 export interface Link {
@@ -12,13 +13,15 @@ interface NavBarProps {
 const NavBar: React.StatelessComponent<NavBarProps> = (props: NavBarProps) => {
   const { links } = props;
   return (
-    <nav className={"navbar-main bg-grey-darkest"}>
-      {links.map(link =>
-        <a className={"nav-item main-nav-link"} href={link.target} role="menuitem">
-          {link.label}
-        </a>
-      )}
-    </nav>
+    <ShadowDOM include={['../styles/index.css']}>
+      <nav className={"navbar-main bg-grey-darkest"}>
+        {links.map(link =>
+          <a className={"nav-item main-nav-link"} href={link.target} role="menuitem">
+            {link.label}
+          </a>
+        )}
+      </nav>
+    </ShadowDOM>
   )
 }
 
